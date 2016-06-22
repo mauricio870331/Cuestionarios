73da7cf5ee93e6691d3a2fe9a99f7d2b7ed4a1d3
clopez <clopez@PC-TEC-02.expresopalmira.local>
2016-06-22 17:33:53 -0500

    conex

--- src/Model/Conexion.java
+++ src/Model/Conexion.java
@@ -9,13 +9,12 @@ public class Conexion {
         Connection con = null;
         try {
             Class.forName("com.mysql.jdbc.Driver").newInstance();
-            con = DriverManager.getConnection("jdbc:mysql://localhost/evaluacion_colegios", "root", "");
+//            con = DriverManager.getConnection("jdbc:mysql://localhost/evaluacion_colegios", "root", "");
+            con = DriverManager.getConnection("jdbc:mysql://www.db4free.net:3306/cuestionarios", "nikol0387", "m1113626301");
 //            conectar = DriverManager.getConnection("jdbc:mysql://www.db4free.net:3306/pm_medex", "mauricio", "openEHR2008");
         } catch (ClassNotFoundException | SQLException | InstantiationException | IllegalAccessException e) {
             JOptionPane.showMessageDialog(null, e);
         }
         return con;
     }
-
-
 }
