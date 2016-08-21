61f49858f1556eb944bb0544e0c179714fc0eca3
Mauricio Herrera <Mauricio Herrera@Maurici-PC>
2016-08-21 16:01:00 -0500

    cuestionario con img ya

--- src/Model/Conexion.java
+++ src/Model/Conexion.java
@@ -7,14 +7,15 @@ public class Conexion {
 
     private static Connection con = null;
 
-    public static Connection getConexion(String clase) {
-        try {
+    public static Connection getConexion(String clase) throws InstantiationException, IllegalAccessException {
+        try {          
+            
             if (con==null) {
-                String driver = "com.mysql.jdbc.Driver"; //el driver varia segun la DB que usemos
-                String url = "jdbc:mysql://190.8.176.243:3306/codigo_proyectos?autoReconnect=true";
+//                String driver = "com.mysql.jdbc.Driver"; //el driver varia segun la DB que usemos
+                String url = "jdbc:mysql://190.8.176.243:3306/codigo_proyectos";
                 String pwd = "P}3LdHI^+WMV";
                 String usr = "codigo_proyectos";
-                Class.forName(driver);
+                Class.forName("com.mysql.jdbc.Driver").newInstance();
                 //con = DriverManager.getConnection("jdbc:mysql://localhost/codigo_proyectos", "codigo_proyectos", "P}3LdHI^+WMV");
                 con = DriverManager.getConnection(url, usr, pwd);
                 System.out.println("Conectionesfull de clase: " + clase);
